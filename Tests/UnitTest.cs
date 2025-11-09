a697d4f7474929180c70b33fd7049e9061e8078f
dimam <dimam@DIMA-PC>
2025-11-09 21:44:33 +0200

    Add project files.

---
+++ Tests/UnitTest.cs
@@ -0,0 +1,140 @@
+using System.IO;
+using System.Xml.Linq;
+using TtFc;
+
+namespace Tests
+{
+    public class UnitTest
+    {
+        //public readonly string FileNameStream = "booksStream.xml";
+        //public readonly string FileName = "books.xml";
+
+        //[Fact]
+        //public void CreateXMLFile()
+        //{
+        //    XMLmanageLibrary.CreateXmlFile(FileName);
+        //    XMLmanageLibrary.CreateXmlFile(FileNameStream);
+        //}
+        [Fact]
+        public void AddNewStream()
+        {
+            string xml = @"<Books>
+                <Book><Name>The Great</Name><Author>Amardilio</Author><Pages>321</Pages></Book>
+            </Books>";
+            var tempPath = Path.GetTempFileName();
+            File.WriteAllText(tempPath, xml);
+
+            using (FileStream stream = new FileStream(tempPath, FileMode.Open, FileAccess.ReadWrite))
+            {
+                stream.Seek(0, SeekOrigin.Begin);
+                var books = new List<TtFc.Models.Book>
+                {
+                new TtFc.Models.Book { Author = "S. King", BookName = "It", Pages = 150 },
+                new TtFc.Models.Book { Author = "A. Sapkovski", BookName = "The Witcher", Pages = 200 },
+                new TtFc.Models.Book { Author = "A. J. M.", BookName = "On The Ground", Pages = 200 },
+                new TtFc.Models.Book { Author = "DD", BookName = "The B", Pages = 200 }
+                };
+
+                XMLmanageLibrary.AddBook(stream, books);
+
+                var test = XMLmanageLibrary.ReadXmlFile(stream);
+
+                Assert.NotNull(test);
+                Assert.True(test.Count > 0);
+                var book = test.FirstOrDefault(a => a.Author == "S. King");
+                Assert.NotNull(book);
+                Assert.Equal("It", book.BookName);
+                Assert.Equal(150, book.Pages);
+                Assert.NotNull(test);
+                Assert.True(test.Count == 5);
+            }
+        }
+        [Fact]
+        public void AddNewFile()
+        {
+            string xml = @"<Books>
+                <Book><Name>The Great</Name><Author>Amardilio</Author><Pages>321</Pages></Book>
+            </Books>";
+            var tempPath = Path.GetTempFileName();
+            File.WriteAllText(tempPath, xml);
+
+            var books = new List<TtFc.Models.Book>
+                {
+                new TtFc.Models.Book { Author = "S. King", BookName = "It", Pages = 150 },
+                new TtFc.Models.Book { Author = "A. Sapkovski", BookName = "The Witcher", Pages = 200 },
+                new TtFc.Models.Book { Author = "A. J. M.", BookName = "On The Ground", Pages = 200 },
+                new TtFc.Models.Book { Author = "DD", BookName = "The B", Pages = 200 }
+                };
+            XMLmanageLibrary.AddBook(tempPath, books);
+            var test = XMLmanageLibrary.ReadXmlFile(tempPath);
+
+            Assert.NotNull(test);
+            Assert.True(test.Count > 0);
+            var book = test.FirstOrDefault(a => a.Author == "S. King");
+            Assert.NotNull(book);
+            Assert.Equal("It", book.BookName);
+            Assert.Equal(150, book.Pages);
+            Assert.NotNull(test);
+            Assert.True(test.Count == 5);
+
+        }
+        [Fact]
+        public void Sort()
+        {
+            string xml = @"<Books>
+                <Book><Name>CC</Name><Author>A</Author><Pages>300</Pages></Book>
+                <Book><Name>BB</Name><Author>A</Author><Pages>200</Pages></Book>
+                <Book><Name>AA</Name><Author>C</Author><Pages>300</Pages></Book>
+            </Books>";
+            var tempPath = Path.GetTempFileName();
+            File.WriteAllText(tempPath, xml);
+
+            XMLmanageLibrary.SortXmlFile(tempPath);
+
+            var result = XElement.Load(tempPath);
+            var names = result.Elements("Book")
+                              .Select(x => (string)x.Element("Name"))
+                              .ToList();
+            var expectedOrder = new List<string> { "BB", "CC", "AA" };
+            Assert.Equal(expectedOrder, names);
+        }
+        [Fact]
+        public void Search()
+        {
+            string xml = @"<Books>
+                <Book><Name>The Man</Name><Author>A</Author><Pages>300</Pages></Book>
+                <Book><Name>The Witcher</Name><Author>A</Author><Pages>200</Pages></Book>
+                <Book><Name>Gwienn</Name><Author>C</Author><Pages>300</Pages></Book>
+            </Books>";
+            var tempPath = Path.GetTempFileName();
+
+            File.WriteAllText(tempPath, xml);
+
+            var result = XMLmanageLibrary.SearchBook(tempPath, "he");
+            Assert.NotNull(result);
+            Assert.True(result.Count > 0);
+            Assert.Equal(2, result.Count);
+        }
+        [Fact]
+        public void SaveTheListOfBooks()
+        {
+            var books = new List<TtFc.Models.Book>
+            {
+                new TtFc.Models.Book { Author = "S. King", BookName = "It", Pages = 150 },
+                new TtFc.Models.Book { Author = "A. Sapkovski", BookName = "The Witcher", Pages = 200 },
+                new TtFc.Models.Book { Author = "A. J. M.", BookName = "On The Ground", Pages = 200 }
+            };
+            var guid = Guid.NewGuid().ToString() + ".xml";
+            XMLmanageLibrary.SaveTheListOfBooks(guid, books);
+            var test = XMLmanageLibrary.ReadXmlFile(guid);
+            Assert.NotNull(test);
+            Assert.True(test.Count == 3);
+            Assert.Equal("S. King", test[0].Author);
+            Assert.Equal("It", test[0].BookName);
+            Assert.Equal(150, test[0].Pages);
+            Assert.Equal("A. J. M.", test[2].Author);
+            Assert.Equal("On The Ground", test[2].BookName);
+            Assert.Equal(200, test[2].Pages);
+        }
+    }
+}
---
+++ TtFc/XMLmanageLibrary.cs
@@ -0,0 +1,173 @@
+﻿using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using TtFc.Models;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace TtFc
+{
+    //https://learn.microsoft.com/en-us/dotnet/api/system.xml.linq.xelement?view=net-8.0
+    //https://learn.microsoft.com/uk-ua/dotnet/standard/linq/sort-elements
+
+    public static class XMLmanageLibrary
+    {
+        private const string Author = "Author";
+        private const string Name = "Name";
+        private const string Pages = "Pages";
+        private const string Book = "Book";
+
+        //public static void CreateXmlFile(string fileName)
+        //{
+        //    if(File.Exists(fileName))
+        //    {
+        //        return;
+        //    }
+        //    XElement books = new XElement("Books");
+        //    XElement book1 = new XElement(Book,
+        //        new XElement(Author, "Kendro"),
+        //        new XElement(Name, "Car Driving"),
+        //        new XElement(Pages, "237")
+        //    );
+
+        //    XElement book2 = new XElement(Book,
+        //        new XElement(Author, "A"),
+        //        new XElement(Name, "A"),
+        //        new XElement(Pages, "300")
+        //    );
+
+        //    books.Add(book1);
+        //    books.Add(book2);
+        //    XDocument doc = new XDocument(books);
+        //    doc.Save(fileName);
+        //}
+        public static List<Book> ReadXmlFile(Stream file)
+        {
+            var load = XElement.Load(file);
+            var books = new List<Book>();
+
+
+            foreach (XElement book in load.Elements(Book))
+            {
+                books.Add(new Book
+                {
+                    Author = book.Element(Author)?.Value,
+                    BookName = book.Element(Name)?.Value,
+                    Pages = int.Parse(book.Element(Pages)?.Value)
+                });
+            }
+            return books;
+        }
+        public static List<Book> ReadXmlFile(string fileName)
+        {
+            var load = XElement.Load(fileName);
+            var books = new List<Book>();
+
+
+            foreach (XElement book in load.Elements(Book))
+            {
+                books.Add(new Book
+                {
+                    Author = book.Element(Author)?.Value,
+                    BookName = book.Element(Name)?.Value,
+                    Pages = int.Parse(book.Element(Pages)?.Value)
+                });
+            }
+            return books;
+        }
+        public static void AddBook(Stream file, List<Book> books)
+        {
+            file.Seek(0, SeekOrigin.Begin);
+            var load = XElement.Load(file);
+            foreach (var book in books)
+            {
+                if (string.IsNullOrWhiteSpace(book.Author) || string.IsNullOrWhiteSpace(book.BookName))
+                {
+                    continue;
+                }
+                XElement newBook = new XElement(Book,
+                    new XElement(Author, book.Author),
+                    new XElement(Name, book.BookName),
+                    new XElement(Pages, book.Pages)
+                );
+                var contains =
+                    from el in load.Elements(Book)
+                    where (string)el.Element(Name)! == book.BookName && (string)el.Element(Author)! == book.Author
+                    select el;
+                if(!contains.Any())
+                {
+                    load.Add(newBook);
+                }
+            }
+            file.SetLength(0);
+            load.Save(file);
+            file.Flush();
+            file.Seek(0, SeekOrigin.Begin);
+        }
+        public static void AddBook(string fileName, List<Book> books)
+        {
+            var load = XElement.Load(fileName);
+            foreach (var book in books)
+            {
+                if (string.IsNullOrWhiteSpace(book.Author) || string.IsNullOrWhiteSpace(book.BookName))
+                {
+                    continue;
+                }
+                XElement newBook = new XElement(Book,
+                    new XElement(Author, book.Author),
+                    new XElement(Name, book.BookName),
+                    new XElement(Pages, book.Pages)
+                );
+                var contains =
+                    from el in load.Elements(Book)
+                    where (string)el.Element(Name)! == book.BookName && (string)el.Element(Author)! == book.Author
+                    select el;
+                if (!contains.Any())
+                {
+                    load.Add(newBook);
+                }
+            }
+            load.Save(fileName);
+        }
+        public static void SortXmlFile(string fileName)
+        {
+            var load = XElement.Load(fileName);
+            var sorted = load.Elements(Book)
+                .OrderBy(el => (string?)el.Element(Author))
+                .ThenBy(el => (string?)el.Element(Name))
+                .ToList();
+            var sortedBooks = new XElement("Books", sorted);
+            sortedBooks.Save(fileName);
+        }
+
+        public static  List<Book> SearchBook(string fileName, string pattern = null)
+        {
+            var load = XElement.Load(fileName);
+            return load.Elements(Book)
+                .Where(x => ((string?)x.Element(Name))?.Contains(pattern, StringComparison.OrdinalIgnoreCase) ?? false)
+                .Select(book => new Book
+                {
+                    Author = book.Element(Author)?.Value,
+                    BookName = book.Element(Name)?.Value,
+                    Pages = int.Parse(book.Element(Pages)?.Value)
+                })
+                .ToList();
+        }
+        public static void SaveTheListOfBooks(string fileName, List<Book> books)
+        {
+            XElement booksXml = new XElement("Books");
+            foreach (var book in books)
+            {
+                XElement newBook = new XElement(Book,
+                    new XElement(Author, book.Author),
+                    new XElement(Name, book.BookName),
+                    new XElement(Pages, book.Pages)
+                );
+                booksXml.Add(newBook);
+            }
+            XDocument doc = new XDocument(booksXml);
+            doc.Save(fileName);
+        }
+        
+
+    }
+}
